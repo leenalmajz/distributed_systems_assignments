3ad8ef9932b2c8df9c45722065a60a153ed0a405
hernyesbotond <botond.hernyes@gmail.com>
2025-05-18 15:43:37 +0200

    queue's done

--- assignment 3/queue_mngr.py
+++ assignment 3/queue_mngr.py
@@ -1,11 +1,11 @@
-from typing import Dict, Any
-import threading, pathlib, json, time, atexit
+from typing import Dict
+import threading, pathlib, json, time, atexit, collections
 
 class Storage():
     def __init__(self, path: str, max_length: int, save_period_time: int):
         self.file = pathlib.Path(path)
         self.max_length = max_length
-        self.queues: Dict[str, Any] = {}
+        self.queues: Dict[str, collections.deque] = {}
 
         self.save_period_time = save_period_time  # in seconds
         self.thread_lock = threading.Lock()
@@ -14,7 +14,7 @@ class Storage():
             with self.file.open() as f:
                 cont = json.load(f)
                 for queue_name, queue_content in cont.items():
-                    self.queues.update({queue_name: queue_content})
+                    self.queues.update({queue_name: collections.deque(queue_content, maxlen = self.max_length)})
 
         t = threading.Thread(target=self.periodical_save, daemon=True)
         t.start()   
@@ -28,30 +28,46 @@ class Storage():
             with self.file.open(mode = "w") as f:
                 json.dump(cont, f)
 
-    def periodical_save(self, queue_name):
+    def periodical_save(self):
         while True:
             time.sleep(self.save_period_time)
             self.save_to_file()
 
     def create_queue(self, queue_name):
         with self.thread_lock:
-            if queue_name in self.queues.keys():
-                return False
-            self.queues
-            return True
+            if queue_name not in self.queues.keys():
+                self.queues.update({queue_name: collections.deque(maxlen = self.max_length)})
+                return True
+            return False
 
-    def list_queue(self):
+    def list_queue_content(self, queue_name):
         with self.thread_lock:
-            return
+            if queue_name not in self.queues.keys():
+                return False
+            return self.queues[queue_name] 
+        
+    def list_queue_names(self):
+        with self.thread_lock:
+            return self.queues.keys()
 
     def delete_queue(self, queue_name):
         with self.thread_lock:
-            return
+            if queue_name in self.queues.keys():
+                self.queues.pop(queue_name)
+                return True
+            return False
 
     def push(self, queue_name, content):
         with self.thread_lock:
-            return
+            queue = self.queues.get(queue_name)
+            if len(queue) >= self.max_length:
+                return False
+            queue.append(content)
+            return True
 
-    def pull(self, queue_name, content):
+    def pull(self, queue_name):
         with self.thread_lock:
-            return
+            queue = self.queues.get(queue_name)
+            if len(queue) <= 0:
+                return None
+            return queue.popleft()
