7b5d416113e4ccb60fe46ca35816ed6a262e44d3
hernyesbotond <botond.hernyes@gmail.com>
2025-05-19 19:05:57 +0200

    asdftgzhjkléá

--- assignment 3/config.py
+++ assignment 3/config.py
@@ -2,5 +2,8 @@ import yaml
 import os
 
 def load_config(path = "config.yaml"):
+    '''
+    Loads the configuration data from a specified yaml file
+    '''
     with open(path) as f:
         return yaml.safe_load(f)
